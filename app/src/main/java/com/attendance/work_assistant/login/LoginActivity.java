e3b7a4ad9fc39291dece8c1a6ce64f6e6d6b5cb0
yujx <yu1057824327@gmail.com>
2018-04-18 19:17:32 +0800

    login activity devlopping

--- app/src/main/java/com/attendance/work_assistant/login/LoginActivity.java
+++ app/src/main/java/com/attendance/work_assistant/login/LoginActivity.java
@@ -6,4 +6,8 @@ import android.support.v7.app.AppCompatActivity;
  * Created by yujx on 2018/4/17.
  */
 public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void setFinishOnTouchOutside(boolean finish) {
+        super.setFinishOnTouchOutside(finish);
+    }
 }
