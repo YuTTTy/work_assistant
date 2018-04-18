3294743c4f6745c58a1d5d4a7b55156d97b2c03a
yujx <yu1057824327@gmail.com>
2018-04-18 17:38:33 +0800

    login 0.1

---
+++ app/src/main/java/com/attendance/work_assistant/login/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.attendance.work_assistant.login;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yujx on 2018/4/17.
+ */
+public class LoginActivity extends AppCompatActivity {
+}
